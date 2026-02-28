e7db19d731412f524c9f580439480486e0abd27b
qualiasolutions <info@qualiasolutions.net>
2026-03-01 01:10:31 +0200

    feat(29-01): split icons.tsx into semantic category files
    - Created 6 category files: navigation, actions, status, brand, content, misc
    - Preserved all 54 icons with exact SVG paths and attributes
    - No barrel index.ts (enables tree-shaking)
    - TypeScript compilation verified
    
    Categories:
    - navigation: 9 icons (HomeIcon, MenuIcon, ChevronDownIcon, etc.)
    - actions: 16 icons (CopyIcon, TrashIcon, UploadIcon, etc.)
    - status: 7 icons (LoaderIcon, CheckCircleFillIcon, ThumbUpIcon, etc.)
    - brand: 2 icons (VercelIcon, GitIcon)
    - content: 12 icons (FileIcon, ImageIcon, MessageIcon, etc.)
    - misc: 8 icons (BotIcon, UserIcon, BoxIcon, etc.)

---
+++ components/icons/misc.tsx
@@ -0,0 +1,176 @@
+import type { SVGProps } from "react";
+
+type IconProps = SVGProps<SVGSVGElement> & {
+	size?: number;
+};
+
+export const BotIcon = () => {
+	return (
+		<svg
+			height="16"
+			strokeLinejoin="round"
+			style={{ color: "currentcolor" }}
+			viewBox="0 0 16 16"
+			width="16"
+		>
+			<path
+				clipRule="evenodd"
+				d="M8.75 2.79933C9.19835 2.53997 9.5 2.05521 9.5 1.5C9.5 0.671573 8.82843 0 8 0C7.17157 0 6.5 0.671573 6.5 1.5C6.5 2.05521 6.80165 2.53997 7.25 2.79933V5H7C4.027 5 1.55904 7.16229 1.08296 10H0V13H1V14.5V16H2.5H13.5H15V14.5V13H16V10H14.917C14.441 7.16229 11.973 5 9 5H8.75V2.79933ZM7 6.5C4.51472 6.5 2.5 8.51472 2.5 11V14.5H13.5V11C13.5 8.51472 11.4853 6.5 9 6.5H7ZM7.25 11.25C7.25 12.2165 6.4665 13 5.5 13C4.5335 13 3.75 12.2165 3.75 11.25C3.75 10.2835 4.5335 9.5 5.5 9.5C6.4665 9.5 7.25 10.2835 7.25 11.25ZM10.5 13C11.4665 13 12.25 12.2165 12.25 11.25C12.25 10.2835 11.4665 9.5 10.5 9.5C9.5335 9.5 8.75 10.2835 8.75 11.25C8.75 12.2165 9.5335 13 10.5 13Z"
+				fill="currentColor"
+				fillRule="evenodd"
+			/>
+		</svg>
+	);
+};
+
+export const UserIcon = ({
+	className,
+	height = 16,
+	width = 16,
+	...props
+}: SVGProps<SVGSVGElement>) => {
+	return (
+		<svg
+			className={className}
+			data-testid="geist-icon"
+			height={height}
+			strokeLinejoin="round"
+			style={{ color: "currentcolor" }}
+			viewBox="0 0 16 16"
+			width={width}
+			{...props}
+		>
+			<path
+				clipRule="evenodd"
+				d="M7.75 0C5.95507 0 4.5 1.45507 4.5 3.25V3.75C4.5 5.54493 5.95507 7 7.75 7H8.25C10.0449 7 11.5 5.54493 11.5 3.75V3.25C11.5 1.45507 10.0449 0 8.25 0H7.75ZM6 3.25C6 2.2835 6.7835 1.5 7.75 1.5H8.25C9.2165 1.5 10 2.2835 10 3.25V3.75C10 4.7165 9.2165 5.5 8.25 5.5H7.75C6.7835 5.5 6 4.7165 6 3.75V3.25ZM2.5 14.5V13.1709C3.31958 11.5377 4.99308 10.5 6.82945 10.5H9.17055C11.0069 10.5 12.6804 11.5377 13.5 13.1709V14.5H2.5ZM6.82945 9C4.35483 9 2.10604 10.4388 1.06903 12.6857L1 12.8353V13V15.25V16H1.75H14.25H15V15.25V13V12.8353L14.931 12.6857C13.894 10.4388 11.6452 9 9.17055 9H6.82945Z"
+				fill="currentColor"
+				fillRule="evenodd"
+			/>
+		</svg>
+	);
+};
+
+export const BoxIcon = ({ size = 16 }: { size: number }) => {
+	return (
+		<svg
+			height={size}
+			strokeLinejoin="round"
+			style={{ color: "currentcolor" }}
+			viewBox="0 0 16 16"
+			width={size}
+		>
+			<path
+				clipRule="evenodd"
+				d="M8 0.154663L8.34601 0.334591L14.596 3.58459L15 3.79466V4.25V11.75V12.2053L14.596 12.4154L8.34601 15.6654L8 15.8453L7.65399 15.6654L1.40399 12.4154L1 12.2053V11.75V4.25V3.79466L1.40399 3.58459L7.65399 0.334591L8 0.154663ZM2.5 11.2947V5.44058L7.25 7.81559V13.7647L2.5 11.2947ZM8.75 13.7647L13.5 11.2947V5.44056L8.75 7.81556V13.7647ZM8 1.84534L12.5766 4.22519L7.99998 6.51352L3.42335 4.2252L8 1.84534Z"
+				fill="currentColor"
+				fillRule="evenodd"
+			/>
+		</svg>
+	);
+};
+
+export const InvoiceIcon = ({ size = 16 }: { size: number }) => {
+	return (
+		<svg
+			height={size}
+			strokeLinejoin="round"
+			style={{ color: "currentcolor" }}
+			viewBox="0 0 16 16"
+			width={size}
+		>
+			<path
+				clipRule="evenodd"
+				d="M13 15.1L12 14.5L10.1524 15.8857C10.0621 15.9534 9.93791 15.9534 9.8476 15.8857L8 14.5L6.14377 15.8922C6.05761 15.9568 5.94008 15.9601 5.85047 15.9003L3.75 14.5L3 15L2.83257 15.1116L1.83633 15.7758L1.68656 15.8756C1.60682 15.9288 1.5 15.8716 1.5 15.7758V15.5958V14.3985V14.1972V1.5V0H3H8H9.08579C9.351 0 9.60536 0.105357 9.79289 0.292893L14.2071 4.70711C14.3946 4.89464 14.5 5.149 14.5 5.41421V6.5V14.2507V14.411V15.5881V15.7881C14.5 15.8813 14.3982 15.9389 14.3183 15.891L14.1468 15.7881L13.1375 15.1825L13 15.1ZM12.3787 5L9.5 2.12132V5H12.3787ZM8 1.5V5V6.5H9.5H13V13.3507L12.7717 13.2138L11.9069 12.6948L11.1 13.3L10 14.125L8.9 13.3L8 12.625L7.1 13.3L5.94902 14.1632L4.58205 13.2519L3.75 12.6972L3 13.1972V1.5H8Z"
+				fill="currentColor"
+				fillRule="evenodd"
+			/>
+		</svg>
+	);
+};
+
+export const LockIcon = ({ size = 16 }: { size?: number }) => {
+	return (
+		<svg
+			height={size}
+			strokeLinejoin="round"
+			style={{ color: "currentcolor" }}
+			viewBox="0 0 16 16"
+			width={size}
+		>
+			<path
+				clipRule="evenodd"
+				d="M10 4.5V6H6V4.5C6 3.39543 6.89543 2.5 8 2.5C9.10457 2.5 10 3.39543 10 4.5ZM4.5 6V4.5C4.5 2.567 6.067 1 8 1C9.933 1 11.5 2.567 11.5 4.5V6H12.5H14V7.5V12.5C14 13.8807 12.8807 15 11.5 15H4.5C3.11929 15 2 13.8807 2 12.5V7.5V6H3.5H4.5ZM11.5 7.5H10H6H4.5H3.5V12.5C3.5 13.0523 3.94772 13.5 4.5 13.5H11.5C12.0523 13.5 12.5 13.0523 12.5 12.5V7.5H11.5Z"
+				fill="currentColor"
+				fillRule="evenodd"
+			/>
+		</svg>
+	);
+};
+
+export const LogsIcon = ({ size = 16 }: { size?: number }) => {
+	return (
+		<svg
+			height={size}
+			strokeLinejoin="round"
+			style={{ color: "currentcolor" }}
+			viewBox="0 0 16 16"
+			width={size}
+		>
+			<path
+				clipRule="evenodd"
+				d="M9 2H9.75H14.25H15V3.5H14.25H9.75H9V2ZM9 12.5H9.75H14.25H15V14H14.25H9.75H9V12.5ZM9.75 7.25H9V8.75H9.75H14.25H15V7.25H14.25H9.75ZM1 12.5H1.75H2.25H3V14H2.25H1.75H1V12.5ZM1.75 2H1V3.5H1.75H2.25H3V2H2.25H1.75ZM1 7.25H1.75H2.25H3V8.75H2.25H1.75H1V7.25ZM5.75 12.5H5V14H5.75H6.25H7V12.5H6.25H5.75ZM5 2H5.75H6.25H7V3.5H6.25H5.75H5V2ZM5.75 7.25H5V8.75H5.75H6.25H7V7.25H6.25H5.75Z"
+				fill="currentColor"
+				fillRule="evenodd"
+			/>
+		</svg>
+	);
+};
+
+export const CpuIcon = ({ size = 16 }: { size?: number }) => (
+	<svg
+		fill="none"
+		height={size}
+		stroke="currentColor"
+		strokeWidth="2"
+		style={{ color: "currentcolor" }}
+		viewBox="0 0 24 24"
+		width={size}
+	>
+		<path
+			d="M4 12C4 8.22876 4 6.34315 5.17157 5.17157C6.34315 4 8.22876 4 12 4C15.7712 4 17.6569 4 18.8284 5.17157C20 6.34315 20 8.22876 20 12C20 15.7712 20 17.6569 18.8284 18.8284C17.6569 20 15.7712 20 12 20C8.22876 20 6.34315 20 5.17157 18.8284C4 17.6569 4 15.7712 4 12Z"
+			strokeLinejoin="round"
+		/>
+		<path d="M9.5 2V4" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M14.5 2V4" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M9.5 20V22" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M14.5 20V22" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M13 9L9 13" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M15 13L13 15" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M22 14.5L20 14.5" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M4 9.5L2 9.5" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M4 14.5L2 14.5" strokeLinecap="round" strokeLinejoin="round" />
+		<path d="M22 9.5L20 9.5" strokeLinecap="round" strokeLinejoin="round" />
+	</svg>
+);
+
+export const PythonIcon = ({ size = 16 }: { size?: number }) => {
+	return (
+		<svg
+			height={size}
+			strokeLinejoin="round"
+			style={{ color: "currentcolor" }}
+			viewBox="0 0 16 16"
+			width={size}
+		>
+			<path
+				d="M7.90474 0.00013087C7.24499 0.00316291 6.61494 0.0588153 6.06057 0.15584C4.42745 0.441207 4.13094 1.0385 4.13094 2.14002V3.59479H7.9902V4.07971H4.13094H2.68259C1.56099 4.07971 0.578874 4.7465 0.271682 6.01496C-0.0826597 7.4689 -0.0983767 8.37619 0.271682 9.89434C0.546012 11.0244 1.20115 11.8296 2.32276 11.8296H3.64966V10.0856C3.64966 8.82574 4.75179 7.71441 6.06057 7.71441H9.91533C10.9884 7.71441 11.845 6.84056 11.845 5.77472V2.14002C11.845 1.10556 10.9626 0.328487 9.91533 0.15584C9.25237 0.046687 8.56448 -0.00290121 7.90474 0.00013087ZM5.81768 1.17017C6.21631 1.17017 6.54185 1.49742 6.54185 1.89978C6.54185 2.30072 6.21631 2.62494 5.81768 2.62494C5.41761 2.62494 5.09351 2.30072 5.09351 1.89978C5.09351 1.49742 5.41761 1.17017 5.81768 1.17017Z"
+				fill="currentColor"
+			/>
+			<path
+				d="M12.3262 4.07971V5.77472C12.3262 7.08883 11.1997 8.19488 9.91525 8.19488H6.06049C5.0046 8.19488 4.13086 9.0887 4.13086 10.1346V13.7693C4.13086 14.8037 5.04033 15.4122 6.06049 15.709C7.28211 16.0642 8.45359 16.1285 9.91525 15.709C10.8868 15.4307 11.8449 14.8708 11.8449 13.7693V12.3145H7.99012V11.8296H11.8449H13.7745C14.8961 11.8296 15.3141 11.0558 15.7041 9.89434C16.1071 8.69865 16.0899 7.5488 15.7041 6.01495C15.4269 4.91058 14.8975 4.07971 13.7745 4.07971H12.3262ZM10.1581 13.2843C10.5582 13.2843 10.8823 13.6086 10.8823 14.0095C10.8823 14.4119 10.5582 14.7391 10.1581 14.7391C9.7595 14.7391 9.43397 14.4119 9.43397 14.0095C9.43397 13.6086 9.7595 13.2843 10.1581 13.2843Z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+};
+
